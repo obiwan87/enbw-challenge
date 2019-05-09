13a7839901f0b12e00dfcd90ee271c77d3e09e6f
Antonino Simone Di Stefano <simon.distefano@gmx.de>
2019-05-09 23:39:43 +0200

    Datetime utils not empty now

---
+++ utils/datetime_utils.py
@@ -0,0 +1,13 @@
+import datetime
+import pandas as pd
+
+# holidays = pd.read_csv('../additional_data/feiertage.csv')
+# holidays.set_index(holidays.Datum)
+
+def day_of_year_to_date(day, year):
+    current_date = datetime.datetime(year, 1, 1) + datetime.timedelta(int(day))
+    return current_date
+
+
+def is_holiday(date):
+    date.strf
