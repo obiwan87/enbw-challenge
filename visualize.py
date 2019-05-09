2f7f27a47a6660f88400bb0cd443cf867f703134
realcoder123 <marco.seidler94@googlemail.com>
2019-05-10 01:44:06 +0200

    Add Visualize

--- visualize.py
+++ visualize.py
@@ -11,7 +11,7 @@ def str2float(x):
     return float(x.replace(',', '.'))
 
 
-def visualize_household(id, start_date, end_date, anomaly_start, anomaly_end):
+def visualize_household(id, start_date, end_date, anomaly_list):
 
     df = pd.read_csv("data/data-sun-holidays.csv", sep=';', na_values=['NA'], converters={'value': str2float})
     df['timestampLocal'] = pd.to_datetime(df.timestampLocal)
@@ -35,27 +35,34 @@ def visualize_household(id, start_date, end_date, anomaly_start, anomaly_end):
     # time range
     g['timestampLocal'] = pd.to_datetime(g['timestampLocal'])
     g = g[(g['timestampLocal'] > start_date) & (g['timestampLocal'] < end_date)]
-    g['anomaly'] = 0
-    g.anomaly[(g['timestampLocal'] > anomaly_start) & (g['timestampLocal'] < anomaly_end)] = 1
-    g.anomaly = g.anomaly * g.value
 
     print(g)
 
     y = g['value'].values
     y_holiday = g['holiday'].values
-    y_anomaly = g['anomaly'].values
 
     x = g['timestampLocal'].values
 
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
-    ax1.plot(x, y, 'g-')
-    ax1.plot(x,y_anomaly, 'r-')
-    ax2.plot(x, y_holiday, 'b-')
+    #fig, ax1 = plt.subplots()
+    #ax2 = ax1.twinx()
+
+    plt.plot(x, y, 'g-', zorder=1)
+    #ax1.plot(x,y_anomaly, 'r-')
+    #ax2.plot(x, y_holiday, 'b-')
+
+    for a in anomaly_list:
+        plt.axvspan(xmin=a[0], xmax = a[1], facecolor='r', alpha=0.5, zorder=2)
+
 
     plt.show()
 
-visualize_household(3, '2014-01-01 00:00:00', '2014-12-31 00:00:00', '2014-01-20 00:00:00', '2014-01-21 00:00:00')
+
+
+
+anomaly_list = [['2014-01-10 00:00:00', '2014-01-21 00:00:00'],
+                ['2014-02-10 00:00:00', '2014-02-21 00:00:00']]
+
+visualize_household(3, '2014-01-01 00:00:00', '2014-12-31 00:00:00', anomaly_list)
 
 
 
